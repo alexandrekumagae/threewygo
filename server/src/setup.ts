03de23ec390c6a63855c4bcb77461c187695d3a4
Alexandre Kumagae <alexandre@movedigital.com.br>
2024-04-15 12:08:39 -0300

    refactor(server): implement new setup

--- server/src/setup.ts
+++ server/src/setup.ts
@@ -1,6 +1,6 @@
 import { pg } from "./lib/postgres";
 
-import moment from "moment";
+import moment, { name } from "moment";
 
 async function setup () {
   await pg.connect()
@@ -39,69 +39,92 @@ async function setup () {
     )`
   )
 
-  // const coursesData = [
-  //   {
-  //     "title": "Fundamentos e Metodologia da Educação Corporativa",
-  //     "slug": "fundamentos-e-metodologia-da-educacao-corporativa",
-  //     "description": "No cenário globalizado, as organizações governamentais têm percebido os servidores públicos como seu principal ativo, o que faz com que a educação corporativa assuma um papel essencial no desenvolvimento e aprimoramento das competências profissionais. Neste curso, aprenda diferentes métodos e estratégias de ensino aplicados à aprendizagem corporativa e contribua para o crescimento organizacional.",
-  //     "expiration_date": moment().add(30, 'days').format('YYYY-MM-DD'),
-  //     "videos": [
-  //       {
-  //         "name": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  //         "path": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  //         "size": 5
-  //       },
-  //       {
-  //         "path": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  //         "size": 10
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     "title": "Tecnologias e Metodologias Inovadoras Aplicadas à Educação Corporativa",
-  //     "slug": "tecnologias-e-metodologias-inovadoras-aplicadas-a-educacao-corporativa",
-  //     "description": "No cenário globalizado, as organizações governamentais têm percebido os servidores públicos como seu principal ativo, o que faz com que a educação corporativa assuma um papel essencial no desenvolvimento e aprimoramento das competências profissionais. Neste curso, aprenda diferentes métodos e estratégias de ensino aplicados à aprendizagem corporativa e contribua para o crescimento organizacional.",
-  //     "expiration_date": moment().add(60, 'days').format('YYYY-MM-DD'),
-  //     "videos": [
-  //       {
-  //         "path": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  //         "size": 5
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     "id": "3",
-  //     "title": "Tecnologias e Metodologias Inovadoras Aplicadas à Educação Corporativa",
-  //     "slug": "tecnologias-e-metodologias-inovadoras-aplicadas-a-educacao-corporativa2",
-  //     "description": "No cenário globalizado, as organizações governamentais têm percebido os servidores públicos como seu principal ativo, o que faz com que a educação corporativa assuma um papel essencial no desenvolvimento e aprimoramento das competências profissionais. Neste curso, aprenda diferentes métodos e estratégias de ensino aplicados à aprendizagem corporativa e contribua para o crescimento organizacional.",
-  //     "expiration_date": moment().add(0, 'days').format('YYYY-MM-DD'),
-  //     "videos": [
-  //       {
-  //         "path": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-  //         "size": 10
-  //       }
-  //     ]
-  //   }
-  // ];
+  const coursesData = [
+    {
+      "title": "Fundamentos e Metodologia da Educação Corporativa",
+      "slug": "fundamentos-e-metodologia-da-educacao-corporativa",
+      "description": "No cenário globalizado, as organizações governamentais têm percebido os servidores públicos como seu principal ativo, o que faz com que a educação corporativa assuma um papel essencial no desenvolvimento e aprimoramento das competências profissionais. Neste curso, aprenda diferentes métodos e estratégias de ensino aplicados à aprendizagem corporativa e contribua para o crescimento organizacional.",
+      "expiration_date": moment().add(30, 'days').format('YYYY-MM-DD'),
+      "videos": [
+        {
+          "name": "SampleVideo_1280x720_2mb.mp4",
+          "path": "SampleVideo_1280x720_2mb.mp4",
+          "size": 2,
+        },
+        {
+          "name": "SampleVideo_1280x720_5mb.mp4",
+          "path": "SampleVideo_1280x720_5mb.mp4",
+          "size": 4
+        },
+        {
+          "name": "SampleVideo_1280x720_2mb.mp4",
+          "path": "SampleVideo_1280x720_2mb.mp4",
+          "size": 2,
+        },
+      ]
+    },
+    {
+      "title": "Tecnologias e Metodologias Inovadoras Aplicadas à Educação Corporativa",
+      "slug": "tecnologias-e-metodologias-inovadoras-aplicadas-a-educacao-corporativa",
+      "description": "No cenário globalizado, as organizações governamentais têm percebido os servidores públicos como seu principal ativo, o que faz com que a educação corporativa assuma um papel essencial no desenvolvimento e aprimoramento das competências profissionais. Neste curso, aprenda diferentes métodos e estratégias de ensino aplicados à aprendizagem corporativa e contribua para o crescimento organizacional.",
+      "expiration_date": moment().add(60, 'days').format('YYYY-MM-DD'),
+      "videos": [
+        {
+          "name": "SampleVideo_1280x720_2mb.mp4",
+          "path": "SampleVideo_1280x720_2mb.mp4",
+          "size": 2,
+        },
+        {
+          "name": "SampleVideo_1280x720_5mb.mp4",
+          "path": "SampleVideo_1280x720_5mb.mp4",
+          "size": 4
+        },
+      ]
+    },
+    {
+      "id": "3",
+      "title": "Educação Corporativa: desenvolvimento e gestão para resultados",
+      "slug": "educacao-corporativa-desenvolvimento-e-gestao-para-resultados",
+      "description": "No cenário globalizado, as organizações governamentais têm percebido os servidores públicos como seu principal ativo, o que faz com que a educação corporativa assuma um papel essencial no desenvolvimento e aprimoramento das competências profissionais. Neste curso, aprenda diferentes métodos e estratégias de ensino aplicados à aprendizagem corporativa e contribua para o crescimento organizacional.",
+      "expiration_date": moment().add(0, 'days').format('YYYY-MM-DD'),
+      "videos": [
+        {
+          "name": "SampleVideo_1280x720_2mb.mp4",
+          "path": "SampleVideo_1280x720_2mb.mp4",
+          "size": 2,
+        },
+        {
+          "name": "SampleVideo_1280x720_5mb.mp4",
+          "path": "SampleVideo_1280x720_5mb.mp4",
+          "size": 4
+        },
+        {
+          "name": "SampleVideo_1280x720_2mb.mp4",
+          "path": "SampleVideo_1280x720_2mb.mp4",
+          "size": 2,
+        },
+      ]
+    }
+  ];
 
-  // for (const course of coursesData) {
-  //   const { title, slug, description, expiration_date, videos } = course;
+  for (const course of coursesData) {
+    const { title, slug, description, expiration_date, videos } = course;
 
-  //   const courseResult = await pg.query(
-  //     `INSERT INTO courses (title, slug, description, expiration_date) VALUES ($1, $2, $3, $4) RETURNING id`,
-  //     [title, slug, description, expiration_date]
-  //   );
+    const courseResult = await pg.query(
+      `INSERT INTO courses (title, slug, description, expiration_date) VALUES ($1, $2, $3, $4) RETURNING id`,
+      [title, slug, description, expiration_date]
+    );
 
-  //   const courseId = courseResult.rows[0].id;
+    const courseId = courseResult.rows[0].id;
 
-  //   for (const video of videos) {
-  //     const { path, size } = video;
-  //     await pg.query(
-  //       `INSERT INTO videos (path, size, course_id) VALUES ($1, $2, $3)`,
-  //       [path, size, courseId]
-  //     );
-  //   }
-  // }
+    for (const video of videos) {
+      const { name, path, size } = video;
+      await pg.query(
+        `INSERT INTO videos (name, path, size, course_id) VALUES ($1, $2, $3, $4)`,
+        [name, path, size, courseId]
+      );
+    }
+  }
 
   console.log('Setup feito com sucesso!')
 

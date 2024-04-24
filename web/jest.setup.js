bf3684d1a873c45780f41ede877d51c56c51f4a0
Alexandre Kumagae <alexandre@movedigital.com.br>
2024-04-24 15:15:19 -0300

    fix: fix to correct VITE_VIDEO_PATH value

--- web/jest.setup.js
+++ web/jest.setup.js
@@ -1,4 +1,4 @@
-global.VITE_VIDEO_PATH = 'http://192.168.15.99:3002/public/videos';
+global.VITE_VIDEO_PATH = 'http://localhost:3002/public/videos';
 
 // Configuração do ts-node
 require('ts-node').register({
